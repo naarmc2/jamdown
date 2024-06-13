7fc340540aef465cd922540879d2af38ce2d048b
naarmc2 <157539735+naarmc2@users.noreply.github.com>
2024-06-13 17:13:34 -0400

    committed

--- dynamics/script.js
+++ dynamics/script.js
@@ -68,27 +68,6 @@
   window.addEventListener("resize", initSlider);
   window.addEventListener("load", initSlider);
 
-//NAVBAR
-    // When the user scrolls the page, execute myFunction
-    window.onscroll = function() {myFunction()};
-
-    // Get the navbar
-    var navbar = document.getElementByClassName("navbar");
-
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
-
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {  
-      if (  window.pageYOffSet || document.documentElement.scrollTop
-        >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
-
-
 
 //FOOTER
    const scriptURL = 'https://script.google.com/macros/s/AKfycbz2ENiuI4yWsjCWBQyjKFZSvopw4ar9x012M0XFAsylvL31RwJRCxP8eYJzl3ZRRWlz/exec'
@@ -98,11 +77,6 @@
      e.preventDefault()
      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
        .then(response => console.log('Success!', response))
-        msg.innerHTML = "Thank You For Subscribing!"
-         setTimeout(function(){
-           msg.innerHTML = ""
-         }, 5000)
-         form.reset()
        .catch(error => console.error('Error!', error.message))
    })
 
